51f678c8d615e341c7ab45caf9313b374dfe9226
RoR Udemy Course <rickandrockin@mail.com>
2017-10-19 21:22:30 +0000

    Add index listing and links to 'show' and 'edit' pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,14 @@
 class ArticlesController < ApplicationController
   
+  def index
+  @all_articles = Article.all
+  end
   def new #so, When you load .../articles/new, you are actually also instantiating a new @article variable.
     @article = Article.new
   end
   
   def edit
+    @article = Article.find(params[:id])
   end
   
   def create
@@ -14,6 +18,16 @@ class ArticlesController < ApplicationController
       redirect_to article_path(@article)
     else
       render 'new'
+    end
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+     flash[:notice] = "Article was successfully updated"
+     redirect_to article_path(@article)
+    else
+    render 'edit'  #render the edit template again if the update hasn’t worked
     end
   end
 

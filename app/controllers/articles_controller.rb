5b25845953564f77d86bc2b14f128cd849b9c2f3
RoR Udemy Course <rickandrockin@mail.com>
2017-10-20 13:12:27 +0000

    extract redundancy in controller, prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  
   def index
   @all_articles = Article.all
   end
@@ -43,6 +45,12 @@ class ArticlesController < ApplicationController
   end
 
   private
+  
+  def set_article
+     @article = Article.find(params[:id])
+  end
+    
+    
   def article_params
     params.require(:article).permit(:title, :description)
   end

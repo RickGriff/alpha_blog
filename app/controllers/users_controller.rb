3c0bb312db6417afac2b9f8a3578530359513e72
RoR Udemy Course <rickandrockin@mail.com>
2017-11-08 21:06:42 +0000

    Add User Signup Controller, View with Form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+  
+  def new
+    @user= User.new
+  end
+
+  def create
+    @user = User.new(user_params) 
+    
+    if @user.save #if it passes validations and hits the DB
+      flash[:success] =  "Welcome to the Alpha Blog #{:username}!"
+      redirect_to articles_path
+    else
+      render 'new'  #if doesnt save, it re-loads the new page.  New page will render any errors that occurred, because that's coded into the New page.
+    end
+    
+  end
+  
+  def user_params   # We need to call a permit method to get user's params.  Params seem to be stored in some general hash, with a user key, then need to permit it's values.
+    params.require(:user).permit(:username, :email, :password)
+  end
+  
+end

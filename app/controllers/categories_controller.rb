5fcf14cf04bc16e4e8bf07eda3d9de15bc93ffbf
RoR Udemy Course <rickandrockin@mail.com>
2017-12-14 21:33:22 +0000

    Add Create and Edit action for Categories, and Edit Category link on Category Show page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -21,6 +21,25 @@ class CategoriesController < ApplicationController
     end
   end
   
+  
+  def edit
+    @category = Category.find(params[:id])
+  
+    
+  end
+  
+  
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)    #update, and if it is successful
+      flash[:success] = "Category name was successfully updated"
+      redirect_to category_path(@category)
+    else    #if update unsuccessful, re-load the edit page
+      render 'edit'
+    end
+  end
+  
+  
   def show
     @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
